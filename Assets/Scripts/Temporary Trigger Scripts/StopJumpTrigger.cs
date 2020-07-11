2f43ef78d6e615015378f730f099d7bc2374453b
Tiago Sambatti <ts_viana@outlook.com>
2020-07-11 17:33:33 -0300

    Tutorial Layout

---
+++ Assets/Scripts/Temporary Trigger Scripts/StopJumpTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopJumpTrigger : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision != null)
+        {
+            if (collision.gameObject == player)
+            {
+                player.GetComponent<PlayerController>().canJump = false;
+            }
+        }
+    }
+}
