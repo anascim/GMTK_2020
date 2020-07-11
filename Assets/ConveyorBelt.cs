a2b12a16508c169066ef130360c68ffd31a374da
anascim <alexjnascim@gmail.com>
2020-07-11 16:20:50 -0300

    Add conveyor belt

---
+++ Assets/ConveyorBelt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float force;
+    public bool goingRight;
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("asd");
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(goingRight ? force : -force, 0f));
+        }
+    }
+}
