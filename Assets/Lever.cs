0288052bf99d5e1fd4d3ea33f0619be3f903116c
anascim <alexjnascim@gmail.com>
2020-07-12 12:27:50 -0300

    Remaining animation changes

---
+++ Assets/Lever.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public Animator animator;
+    private bool activated = false;
+
+    public void Toggle()
+    {
+        activated = !activated;
+        if (activated)
+            animator.SetTrigger("Activate");
+        else
+            animator.SetTrigger("Deactivate");
+    }
+}
